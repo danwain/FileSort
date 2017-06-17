bae9b405d750ac2c684a8040f8ed40c72df351ca
Daniel Wainwright <dan.wainwright2@gmail.com>
2017-06-17 17:20:38 +0100

    Update inital sort

--- src/uk/ac/cam/daw87/oop/summer/Sort/External.java
+++ src/uk/ac/cam/daw87/oop/summer/Sort/External.java
@@ -7,6 +7,7 @@ import uk.ac.cam.daw87.oop.summer.Sort.helpers.Position;
 import java.io.*;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.*;
 
 public class External {
@@ -27,11 +28,11 @@ public class External {
     }
 
     public void sort() throws IOException, IllegalArgumentException{
-        System.out.println(Helper.FileToString(f1));
-        initialSort(f1,f2,size);
-        System.out.println(Helper.FileToString(f2));
-        merge();
-        System.out.println(Helper.FileToString(f1));
+        //System.out.println(Helper.FileToString(f1));
+        initialSort(f1.getChannel(),f2.getChannel(), ByteBuffer.allocate(size*4));
+        //System.out.println(Helper.FileToString(f2));
+        //merge();
+        //System.out.println(Helper.FileToString(f1));
     }
 
     private void merge() throws IOException{
@@ -81,26 +82,26 @@ public class External {
             f1.writeInt(write[i]);
     }
 
-    private void initialSort(RandomAccessFile f1, RandomAccessFile f2, int size) throws IOException{
-        int[] temp = new int[size];
-        int index = 0;
-        f1.seek(0);
-        f2.seek(0);
-        while ((index + size) < Total_ints){
-            for (int i = 0; i < size; i++)
-                temp[i] = f1.readInt();
+    private void initialSort(FileChannel f1, FileChannel f2, ByteBuffer buffer) throws IOException{
+        //TODO: Sort better without creating an array ie using only the buffer then write the buffer.
+        f1.position(0);
+        f2.position(0);
+        int length;
+        while (true){
+            buffer.position(0);
+            length = f1.read(buffer);
+            if (length <= 0)
+                break;
+            int[] temp = new int[length / 4];
+            for (int i = 0; i < length / 4; i++)
+                temp[i] = buffer.getInt(i*4);
             Arrays.sort(temp);
+            int start = buffer.capacity() - length;
+            buffer.position(start);
             for (int i : temp)
-                f2.writeInt(i);
-            index += size;
-        }
-        if (index < Total_ints){
-            int[] shorter = new int[Total_ints - index];
-            for (int i = 0; i < (Total_ints - index) ; i++)
-                shorter[i] = f1.readInt();
-            Arrays.sort(shorter);
-            for (int i: shorter)
-                f2.writeInt(i);
+                buffer.putInt(i);
+            buffer.position(start);
+            f2.write(buffer);
         }
     }
 }
