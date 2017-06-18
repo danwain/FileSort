aed04ebadfdb36dfb65179eba23c5144a0f7e187
Daniel Wainwright <dan.wainwright2@gmail.com>
2017-06-18 12:25:26 +0100

    Change to use filechanel

--- src/uk/ac/cam/daw87/oop/summer/Sort/External.java
+++ src/uk/ac/cam/daw87/oop/summer/Sort/External.java
@@ -11,56 +11,58 @@ import java.util.*;
 
 public class External {
     private final int size; //Array Size
-    private final RandomAccessFile f1;
-    private final RandomAccessFile f2;
+    private final String to;
+    private final String from;
+    private final FileChannel f1;
+    private final FileChannel f2;
     private final int Total_ints;
     private final int groups;
     private final Set<Position> positions;
 
     public External(int size, String p1, String p2) throws FileNotFoundException, IOException{
         this.size = size;
-        this.f1 = new RandomAccessFile(p1,"rw");
-        this.f2 = new RandomAccessFile(p2,"rw");
-        this.Total_ints = (int) f1.length() / 4;
+        this.from = p1;
+        this.to = p2;
+        this.f1 = new RandomAccessFile(p1,"rw").getChannel();
+        this.f2 = new RandomAccessFile(p2,"rw").getChannel();
+        this.Total_ints = (int) f1.size() / 4;
         this.groups = Helper.roundUp(Total_ints, size);
         this.positions = new LinkedHashSet<>();
     }
 
     public void sort() throws IOException, IllegalArgumentException{
-        //System.out.println(Helper.FileToString(f1));
-        initialSort(f1.getChannel(),f2.getChannel(), ByteBuffer.allocate(size*4));
-        //System.out.println(Helper.FileToString(f2));
-        //merge();
-        //System.out.println(Helper.FileToString(f1));
+        //System.out.println(Helper.FileToString(new RandomAccessFile(from,"rw")));
+        //initialSort(f1,f2, ByteBuffer.allocate(size*4));
+        //System.out.println(Helper.FileToString(new RandomAccessFile(to,"rw")));
+        merge();
+        //System.out.println(Helper.FileToString(new RandomAccessFile(from,"r")));
     }
 
-    private void merge() throws IOException{
-        f2.seek(0);
+    public void merge() throws IOException{
+        f2.position(0);
         for (int i = 0; i < groups - 1 ; i++)
-            positions.add(new Position(i*size,size,f2.getChannel()));
-        positions.add(new Position(positions.size()*size,Total_ints - (positions.size()*size),f2.getChannel()));
+            positions.add(new Position(i*size,size,f2));
+        positions.add(new Position(positions.size()*size,Total_ints - (positions.size()*size),f2));
 
         Queue<Number> queue = new PriorityQueue<>();
         Map<Position, Integer> amounts = new HashMap<>();
-        int[] write = new int[size / 2];
+        ByteBuffer write = ByteBuffer.allocate(1+(size / 2) - (size / 2) % 4);
         int amount_in_each = (size / 2) / groups;
         if (amount_in_each == 0)
             amount_in_each = 1;
         ByteBuffer buffer = ByteBuffer.allocate(amount_in_each * 4);
         for (Position p : positions){
-            //Set<Number> toAdd = p.getNext(amount_in_each);
             int length = p.getNext(amount_in_each, buffer);
             for (int i = 0; i < length; i++)
                 queue.add(new Number(buffer.getInt(i*4),p));
             amounts.put(p,length);
         }
-        f1.seek(0);
-        int index = 0;
+        f1.position(0);
         while (!queue.isEmpty()){
-            if (index == write.length){
-                for (int i : write)
-                    f1.writeInt(i);
-                index = 0;
+            if (write.position() + 4 > write.capacity()){
+                write.flip();
+                f1.write(write);
+                write.clear();
             }
             Number n = queue.poll();
             assert amounts.get(n.group) > 0;
@@ -74,11 +76,10 @@ public class External {
             } else {
                 amounts.put(n.group, amounts.get(n.group) - 1);
             }
-            write[index] = n.number;
-            index++;
+            write.putInt(n.number);
         }
-        for (int i = 0; i < index ; i++)
-            f1.writeInt(write[i]);
+        write.flip();
+        f1.write(write);
     }
 
     private void initialSort(FileChannel f1, FileChannel f2, ByteBuffer buffer) throws IOException{
@@ -93,7 +94,8 @@ public class External {
                 break;
             buffer.position(0);
             BufferSortQuickSort.quickSort(buffer.asIntBuffer(),0,length/4);
-            buffer.flip();
+            buffer.position(0);
+            buffer.limit(length);
             f2.write(buffer);
         }
     }
