7273a3df01d285ffffa5c3680da69255929d0597
Daniel Wainwright <dan.wainwright2@gmail.com>
2017-07-30 15:50:49 +0100

    Add main sort class
    Add the class the uni tester needs

---
+++ src/uk/ac/cam/daw87/fjava/tick0/ExternalSort.java
@@ -0,0 +1,57 @@
+package uk.ac.cam.daw87.fjava.tick0;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import uk.ac.cam.daw87.oop.summer.Sort.External;
+
+public class ExternalSort {
+
+    public static void sort(String f1, String f2) throws FileNotFoundException, IOException {
+        //TODO: Find best size of merge
+        External e = new External(9000, f1, f2);
+        e.sort();
+    }
+
+    private static String byteToHex(byte b) {
+        String r = Integer.toHexString(b);
+        if (r.length() == 8) {
+            return r.substring(6);
+        }
+        return r;
+    }
+
+    public static String checkSum(String f) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            DigestInputStream ds = new DigestInputStream(
+                    new FileInputStream(f), md);
+            byte[] b = new byte[512];
+            while (ds.read(b) != -1)
+                ;
+
+            String computed = "";
+            for(byte v : md.digest())
+                computed += byteToHex(v);
+
+            return computed;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "<error computing checksum>";
+    }
+
+    public static void main(String[] args) throws Exception {
+        String f1 = args[0];
+        String f2 = args[1];
+        sort(f1, f2);
+        System.out.println("The checksum is: "+checkSum(f1));
+    }
+}
