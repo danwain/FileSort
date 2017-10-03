d8e49ed069638c9b8d3f30be5d14fc886a114f27
Daniel Wainwright <dan.wainwright2@gmail.com>
2017-10-04 00:21:20 +0100

    Add methods to Sorters

--- src/uk/ac/cam/daw87/fjava/tick0/helpers/Sorters.java
+++ src/uk/ac/cam/daw87/fjava/tick0/helpers/Sorters.java
@@ -42,26 +42,61 @@ public class Sorters {
         quickSort(i,0,i.capacity());
     }
 
-    public final static void quickSort(IntBuffer buffer, int start, int end){
-        assert buffer != null;
-        if (end - start <= 1)
-            return;
-
-        int m = partition(buffer, start, end);
-        quickSort(buffer, start, m);
-        quickSort(buffer, m+1, end);
+    private static boolean probablysorted(IntBuffer buffer, int start, int end){
+        int first = buffer.get(start);
+        int second = buffer.get(start + 1);
+        int thrid = buffer.get(start + 2);
+        int fourth = buffer.get(start + 3);
+        return first <= second && second <= thrid && thrid <= fourth;
     }
 
-    private final static void swapp(IntBuffer buffer, int a, int b){
+    public static void quickSort(IntBuffer buffer, int start, int end){
+        int length = end - start;
+        if (length < 7 || probablysorted(buffer, start, end)){
+            insertSort(buffer, start, end);
+        } else {
+            quickSortInternal(buffer, start, end);
+        }
+    }
+
+    private static void quickSortInternal(IntBuffer buffer, int start, int end){
+        assert buffer != null;
+        if (end - start < 7 || probablysorted(buffer, start, end)) {
+            insertSort(buffer, start, end);
+        } else {
+            int m = partition(buffer, start, end);
+            quickSortInternal(buffer, start, m);
+            quickSortInternal(buffer, m + 1, end);
+        }
+    }
+
+    private static void swapp(IntBuffer buffer, int a, int b){
         int temp = buffer.get(a);
         buffer.put(a, buffer.get(b));
         buffer.put(b, temp);
     }
 
-    private final static int partition(IntBuffer buffer, int start, int end){
+    private static int median(IntBuffer buffer, int start, int end){
+        int first = buffer.get(start);
+        int last = buffer.get(end - 1);
+        int middle = buffer.get((start + end - 1) >> 1);
+
+        int x = first-last;
+        int y = last-middle;
+        int z = first-middle;
+        if (x*y > 0){
+            return end - 1;
+        } else if(x*z > 0) {
+            return (start + end - 1) >> 1;
+        } else {
+            return start;
+        }
+    }
+
+    private static int partition(IntBuffer buffer, int start, int end){
         int il = start;
         int ir = end - 1;
-        int average = (il +ir) >> 1;
+        int average = median(buffer, start, end);
         swapp(buffer, average, ir);
         average = ir;
         //assert (average <= ir && average >= il);
@@ -80,5 +115,64 @@ public class Sorters {
         }
         buffer.put(il,pivot);
         return il;
+    }
+
+    private static void insertSort(IntBuffer buffer, int start, int end){
+        int temp;
+        int k;
+        for (int i = start + 1; i < end; i++) {
+            temp = buffer.get(i);
+            for (k = i - 1; k >= 0 && temp < buffer.get(k); k--){
+                buffer.put(k + 1, buffer.get(k));
+            }
+            buffer.put(k + 1, temp);
+        }
+    }
+
+
+    public static void RadixSort(byte[] a, int start, int end) {
+        final int R = 1 << 8;    // each bytes is between 0 and 255
+        final int MASK = R - 1;              // 0xFF
+        final int w = 4;  // each int is 4 bytes
+
+        int n = end - start;
+        byte[] aux = new byte[n];
+
+        for (int d = w-1; d >= 0; d--) {
+
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = d; i < n; i+=4) {
+                int c = a[i + start] & MASK;
+                count[c + 1]++;
+            }
+
+            // compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+
+            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+            if (d == 0) {
+                int shift1 = count[R] - count[R/2];
+                int shift2 = count[R/2];
+                for (int r = 0; r < R/2; r++)
+                    count[r] += shift1;
+                for (int r = R/2; r < R; r++)
+                    count[r] -= shift2;
+            }
+
+            // move data
+            for (int i = d; i < n; i+=4) {
+                int c = a[i + start] & MASK;
+                for (int j = 0; j < 4 ; j++) {
+                    aux[count[c] * 4 + j] = a[i + j - d + start];
+                }
+                count[c]++;
+            }
+
+            // copy back
+            for (int i = 0; i < n; i++)
+                a[i + start] = aux[i];
+        }
     }
 }
